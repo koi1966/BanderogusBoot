2455d2d73c5c71ef68c44ad6dde3db7873a1c38f
koi1966 <koi196621@gmail.com>
2023-03-30 21:52:34 +0300

    Chat Telegram 30.03.2023

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,7 +29,8 @@ import java.util.*;
 //        на message.setText(text);
 public class Main extends TelegramLongPollingBot {
 
-    private Map<Long,Integer> levels = new HashMap<>();
+    private Map<Long, Integer> levels = new HashMap<>();
+
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
         api.registerBot(new Main());
@@ -50,9 +51,9 @@ public class Main extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
         Long chatId = getChatId(update);
-        if (update.hasMessage() && update.getMessage().getText().equals("/start")){
+        if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
             // Send imege level 1
-            sendImage("level-1",chatId);
+            sendImage("level-1", chatId);
 
             // Send message
             SendMessage messages = createMessages("Ґа-ґа-ґа!\n" +
@@ -72,52 +73,113 @@ public class Main extends TelegramLongPollingBot {
                     "Обери завдання, щоб перейти на наступний рівень");
             messages.setChatId(chatId);
 
+            List<String> battons = Arrays.asList(
+                    "Сплести маскувальну сітку (+15 монет)",
+                    "Зібрати кошти патріотичними піснями (+15 монет)",
+                    "Вступити в Міністерство Мемів України (+15 монет)",
+                    "Запустити волонтерську акцію (+15 монет)",
+                    "Вступити до лав тероборони (+15 монет)"
+            );
+
+            battons = getRandom3(battons);
+
             attachButtons(messages, Map.of(
-                    "Сплести маскувальну сітку (+15 монет)", "level_1_task",
-                    "Зібрати кошти патріотичними піснями (+15 монет)","level_1_task",
-                    "Вступити в Міністерство Мемів України (+15 монет)","level_1_task"
+                    battons.get(0), "level_1_task",
+                    battons.get(1), "level_1_task",
+                    battons.get(2), "level_1_task"
             ));
             sendApiMethodAsync(messages);
         }
 
         if (update.hasCallbackQuery()) {
-            if (update.getCallbackQuery().getData().equals("level_1_task") && getLevel(chatId)== 1) {
+            if (update.getCallbackQuery().getData().equals("level_1_task") && getLevel(chatId) == 1) {
                 // increase level
-                setLevels(chatId,2);
-            // Send image
-                sendImage("level-2",chatId);
+                setLevels(chatId, 2);
+                // Send image
+                sendImage("level-2", chatId);
                 // Send message
                 SendMessage messages = createMessages("*Вітаємо на другому рівні! Твій гусак - біогусак.*\n" +
                         "Баланс: 20 монет. \n" +
                         "Обери завдання, щоб перейти на наступний рівень");
                 messages.setChatId(chatId);
 
-                attachButtons(messages,Map.of(
-                        "Зібрати комарів для нової біологічної зброї (+15 монет)", "level_2_task",
-                        "Пройти курс молодого бійця (+15 монет)", "level_2_task",
-                        "Задонатити на ЗСУ (+15 монет)", "level_2_task"));
-                sendApiMethodAsync(messages);
+                List<String> battons = Arrays.asList(
+                        "Зібрати комарів для нової біологічної зброї (+15 монет)",
+                        "Пройти курс молодого бійця (+15 монет)",
+                        "Задонатити на ЗСУ (+15 монет)",
+                        "Збити дрона банкою огірків (+15 монет)",
+                        "Зробити запаси коктейлів Молотова (+15 монет)"
+                );
 
+                battons = getRandom3(battons);
+
+                attachButtons(messages, Map.of(
+                        battons.get(0), "level_2_task",
+                        battons.get(1), "level_2_task",
+                        battons.get(2), "level_2_task"
+                ));
+                sendApiMethodAsync(messages);
             }
         }
 
         if (update.hasCallbackQuery()) {
-            if (update.getCallbackQuery().getData().equals("level_2_task") && getLevel(chatId)== 2) {
+            if (update.getCallbackQuery().getData().equals("level_2_task") && getLevel(chatId) == 2) {
                 // increase level
-                setLevels(chatId,3);
+                setLevels(chatId, 3);
                 // Send image
-                sendImage("level-3",chatId);
+                sendImage("level-3", chatId);
                 // Send message
                 SendMessage messages = createMessages("*Вітаємо на третьому рівні! Твій гусак - бандеростажер.*\n" +
                         "Баланс: 35 монет. \n" +
                         "Обери завдання, щоб перейти на наступний рівень");
                 messages.setChatId(chatId);
 
-                attachButtons(messages,Map.of(
-                        "Злітати на тестовий рейд по чотирьох позиціях (+15 монет)", "level_3_task",
-                        "Відвезти гуманітарку на передок (+15 монет)", "level_3_task",
-                        "Задонатити на ЗСУ (+15 монет)", "level_3_task"));
+                List<String> battons = Arrays.asList(
+                        "Злітати на тестовий рейд по чотирьох позиціях (+15 монет)",
+                        "Відвезти гуманітарку на передок (+15 монет)",
+                        "Знайти зрадника та здати в СБУ (+15 монет)",
+                        "Навести арту на орків (+15 монет)",
+                        "Притягнути танк трактором (+15 монет)"
+                );
+
+                battons = getRandom3(battons);
+
+                attachButtons(messages, Map.of(
+                        battons.get(0), "level_3_task",
+                        battons.get(1), "level_3_task",
+                        battons.get(2), "level_3_task"));
                 sendApiMethodAsync(messages);
+            }
+        }
+
+        if (update.hasCallbackQuery()) {
+            if (update.getCallbackQuery().getData().equals("level_3_task") && getLevel(chatId) == 3) {
+                setLevels(chatId, 4);
+                sendImage("level-4", chatId);
+
+                SendMessage messages = createMessages("*Вітаємо на останньому рівні! Твій гусак - готова біологічна зброя - бандерогусак.*\n" +
+                        "Баланс: 50 монет. \n" +
+                        "Тепер ти можеш придбати Джавелін і глушити чмонь");
+                messages.setChatId(chatId);
+
+                List<String> battons = Arrays.asList(
+                        "Купити Джавелін (50 монет)"
+                );
+
+                attachButtons(messages, Map.of(
+                        battons.get(0), "level_4_task"
+                        ));
+                sendApiMethodAsync(messages);
+            }
+        }
+
+        if (update.hasCallbackQuery()) {
+            if (update.getCallbackQuery().getData().equals("level_4_task") && getLevel(chatId) == 4) {
+                setLevels(chatId, 5);
+                SendMessage messages = createMessages("*Джавелін твій. Повний вперед!*");
+                messages.setChatId(chatId);
+
+                sendImage("final", chatId);
 
             }
         }
@@ -139,11 +201,12 @@ public class Main extends TelegramLongPollingBot {
 //        3)а в методі createMessage змініть
 //        message.setText(new String(text.getBytes(), StandardCharsets.UTF_8));
 //        на message.setText(text);
-        message.setText(new String(text.getBytes(), StandardCharsets.UTF_8) );
+        message.setText(new String(text.getBytes(), StandardCharsets.UTF_8));
         message.setParseMode("markdown");
 //        message.setParseMode("markdown");
         return message;
     }
+
     public void attachButtons(SendMessage message, Map<String, String> buttons) {
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
 
@@ -155,7 +218,7 @@ public class Main extends TelegramLongPollingBot {
             String buttonValue = buttons.get(buttonName);
 
             InlineKeyboardButton button = new InlineKeyboardButton();
-            button.setText(new String( buttonName.getBytes(), StandardCharsets.UTF_8) );
+            button.setText(new String(buttonName.getBytes(), StandardCharsets.UTF_8));
             button.setCallbackData(buttonValue);
 
             keyboard.add(Arrays.asList(button));
@@ -164,22 +227,29 @@ public class Main extends TelegramLongPollingBot {
         message.setReplyMarkup(markup);
     }
 
-    public void sendImage(String name, Long chatId){
+    public void sendImage(String name, Long chatId) {
         SendAnimation animation = new SendAnimation();
 
         InputFile inputFile = new InputFile();
-        inputFile.setMedia(new File("images/" + name+ ".gif"));
+        inputFile.setMedia(new File("images/" + name + ".gif"));
 
         animation.setAnimation(inputFile);
         animation.setChatId(chatId);
         executeAsync(animation);
     }
 
-    public int getLevel(Long chatId){
-        return levels.getOrDefault(chatId,1);
+    public int getLevel(Long chatId) {
+        return levels.getOrDefault(chatId, 1);
     }
 
-    public void setLevels(Long chatId, int level){
-        levels.put(chatId,level);
+    public void setLevels(Long chatId, int level) {
+        levels.put(chatId, level);
+    }
+
+    public List<String> getRandom3(List<String> variants) {
+        ArrayList<String> copy = new ArrayList<>(variants);
+        Collections.shuffle(copy);
+        return copy.subList(0, 3);
+
     }
 }
